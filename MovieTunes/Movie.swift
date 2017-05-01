b9d73c8fabde1ed1dc59df801fc8609630c23c32
Buka Cakrawala <kaka10garuda@gmail.com>
2017-05-01 13:18:11 -0700

    perform networking process

---
+++ MovieTunes/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  MovieTunes
+//
+//  Created by Buka Cakrawala on 5/1/17.
+//  Copyright © 2017 Buka Cakrawala. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+
+    var title: String!
+    var releasedDate: String!
+    var price: String!
+    var coverImageURL: String!
+    var itunesURL: String!
+
+}
--- MovieTunes/ViewController.swift
+++ MovieTunes/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var movies = [Movie]()
+    
+    var links = NSArray()
+    
+    var images: NSArray!
+    var imgageAttribute: [String: AnyObject]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // MARK: - Networking Request
+        Alamofire.request("https://itunes.apple.com/us/rss/topmovies/limit=25/json").responseJSON { (response) in
+            if let json = response.result.value as? [String: AnyObject] {
+                let feed: [String: AnyObject] = json["feed"] as! [String: AnyObject]
+                let entries = feed["entry"] as! NSArray
+                
+                for (index, entry) in entries.enumerated() {
+                    
+                    let movie = Movie()
+                    
+                    let dict = entry as! [String: AnyObject]
+                    
+                    if let link = dict["id"]?["label"] {
+                        movie.itunesURL = link as! String
+                    }
+                    
+                    if let title = dict["im:name"]?["label"] {
+                        movie.title = title as! String
+                    } else {
+                        movie.title = "No Available"
+                    }
+                    
+                    if let price = dict["im:price"]?["label"] {
+                        movie.price = price as! String
+                    } else {
+                        movie.price = "$0.00"
+                    }
+                    
+                    if let date = dict["im:releaseDate"]?["label"] {
+                        movie.releasedDate = date as! String
+                    }
+                    
+                    
+                    self.images = dict["im:image"] as! NSArray
+                    self.imgaeAttribute = self.images[0] as [String: AnyObject]
+                    
+                    if let imageURL =
+                    
+                    self.movies.append(movie)
+                    return
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +73,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension ViewController {
+    
 }
 
